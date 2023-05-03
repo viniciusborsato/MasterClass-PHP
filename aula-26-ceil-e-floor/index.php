6f9c283200df20f1b190fdec451fcacbeae9c66a
Vinicius Borsato <vinicius@viniciusborsato.com>
2023-05-03 15:43:33 -0300

    Add desc

--- aula-26-ceil-e-floor/index.php
+++ aula-26-ceil-e-floor/index.php
@@ -1,4 +1,9 @@
 <?php
+	// Aula 26 - ceil() e floor()
+	// Como informado, exemplo simples, direto da documentação
+	// O conteúdo completo está na aula
+	// vinicius@viniciusborsato.com
+	
 	// "ceil" arredonda frações para cima
 	echo ceil(4.3) . "<br>";    // 5
 	echo ceil(9.999) . "<br>";  // 10
